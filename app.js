077fad050694fd5ce57964415e354816c86a2499
Ru-Ri-z <69484119+Ru-Ri-z@users.noreply.github.com>
2021-04-09 14:37:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
         squares [appleIndex].classList.remove('apple')
         clearInterval(interval)
         score = 0
+        randomApple()
         direction = 1
         scoreDisplay.innerText = score
         intervalTime = 1000
@@ -28,16 +29,45 @@ document.addEventListener('DOMContentLoaded', () => {
         interval = setInterval(moveOutcomes, intervalTime)
     }
 
+    function moveOutcomes() {
 
 
+        if (
+            (currentSnake[0] + width >= (width * width) && direction === width ) ||
+            (currentSnake[0] % width === width -1 && direction === 1) ||
+            (currentSnake[0] % width === 0 && direction === -1) ||
+            (currentSnake[0] - width < 0 && direction === -width) ||
+            squares[currentSnake[0] + direction].classList.contains('snake')
+        ) {
+            return clearInterval(interval)
+
+            const tail = currentSnake.pop()
+            squares[tail].classList.remove('snake')
+            currentSnake.unshift(currentSnake[0] + direction)
+
+            if(squares[currentSnake[0]].classList.contains('apple')) {
+                squares[currentSnake[0]].classList.remove('apple')
+                squares[tail].classList.add('snake')
+                currentSnake.push(tail)
+                randomApple()
+                score++
+                scoreDisplay.textContent = score
+                clearInterval(interval)
+                intervalTime = intervalTime * speed
+                interval = setInterval(moveOutcomes, intervalTime)
+            }
+            squares[currentSnake[0]].classList.add('snake')
+        }
+
+    }
 
 
-
-
-
-
-
-
+    function randomApple() {
+        do{
+            appleIndex = Math.floor(Math.random() * squares.lenght)
+        }   while(squares[appleIndex].classList.contains('snake'))
+        squares[appleIndex].classList.add('apple')
+    }
 
 
     function control(e) {
@@ -48,7 +78,7 @@ document.addEventListener('DOMContentLoaded', () => {
         } else if (e.keycode === 38) {
             direction = -width
         } else if (e.keycode === 37) {
-            direction -1
+            direction = -1
         } else if (e.keycode === 40) {
             direction = +width
         }
@@ -56,5 +86,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     document.addEventListener('keyup', control)
+    startBtn.addEventListener('click', startGame)
 
 })
